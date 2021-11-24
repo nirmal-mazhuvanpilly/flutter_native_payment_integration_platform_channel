4a51df3ce4d7fbedc95927c7e1ae9537449832b6
nirmal-mazhuvanpilly <nirmalmazhuvanpilly@gmail.com>
2021-11-24 12:47:11 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Tamara Payment Integration'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? status;
+
+  startPayment() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(status??"Status not available",style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            ElevatedButton(
+              child: const Text("Start Payment"),
+              onPressed: startPayment,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
